bf6dd542a8c35ef822d4cae77514a61d40538dd8
nxelia99 <55344006+nxelia99@users.noreply.github.com>
2022-07-18 12:58:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+/* Dark Mode */
+
+
+
+/*Scrollbar */
+
+
+/** videos */
+$(document).ready(function () {
+    $(".video-gallery").magnificPopup({
+      delegate: "a",
+      type: "iframe",
+      gallery: {
+        enabled: true
+      }
+    });
+  });
+  

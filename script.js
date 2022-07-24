1f54a8a25a600bcd28a3864cbed5c779611c3619
Nxelia <55344006+nxelia99@users.noreply.github.com>
2022-07-24 19:40:01 +0200

    added dark mode change

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 /* Dark Mode */
 
 
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+  document.body.classList.toggle('dark');
+})
 
 /*Scrollbar */
 
